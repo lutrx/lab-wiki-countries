73d3d046113670fff41fc55faaeb25d5aa0eb88c
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-17 19:15:25 +0100

    Iteration3 use axios to get data instead of json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import {Route, Routes} from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CountriesList from './components/CountriesList';
 import Navbar from './components/Navbar';
-import countriesData from './countries.json';
+//import countriesData from './countries.json';
 import CountryDetails from './components/CountryDetails';
 
 function App() {
-  const [countries, setCountries] = useState(countriesData);
+const [countries, setCountries] = useState([])
+useEffect(() => {
+      axios.get('https://ih-countries-api.herokuapp.com/countries')
+        .then((response) => {
+          setCountries(response.data)
+        })
+}, [])
+
+  
   return (
     <div className="App">
       <Navbar />
