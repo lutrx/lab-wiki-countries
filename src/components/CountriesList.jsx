3d7dacdf6c9192d56eaf143f6ac1624a520e2454
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-17 17:57:21 +0100

    Show flag images in countries list

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -6,7 +6,7 @@ function CountriesList({countries}) {
             return (
                 <div key={country.alpha3Code} className="col-5">
                     <div className="list-group">
-                        <Link to={country.alpha3Code} className="list-group-item list-group-item-action">{country.name.common} </Link>    
+                        <Link to={country.alpha3Code} className="list-group-item list-group-item-action"> <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="flag" /> {country.name.common} </Link>    
                     </div>
                 </div>
             )
